eefa0de12dcb88226cee10cb2bec38410cf55d29
Nick <nikita.stets.999@gmail.com>
2020-08-26 11:31:19 +0300

    IHostService as BackgroundTask

--- NetCoreCheckDemo/NetCoreCheckDemo/HostService.cs
+++ NetCoreCheckDemo/NetCoreCheckDemo/HostService.cs
@@ -47,48 +47,58 @@ namespace NetCoreCheckDemo
 
         public virtual Task StartAsync(CancellationToken cancellationToken)
         {
-            return Task.Factory.StartNew(async () =>
-            {
-                while (!cancellationToken.IsCancellationRequested)
-                {
-                    using (_logger.BeginScope("Execute Async has started"))
-                    {
-                        var root = (IConfigurationRoot)_configuration;
-                        var jsonProvider = root.Providers.ToArray()[1] as JsonConfigurationProvider;
-                        jsonProvider.TryGet("SomeValue", out string value);
-                        var someVAlue = _configuration.GetValue<int>("SomeValue");
-                        var timespan = new TimeSpan(0, 0, 0, 30);
-                        await Task.Delay(timespan);
-                        var test2Age = _testSettings.Get("Test2").TestAge;
-                        var test1Age = _testSettings.Get("Test1").TestAge;
-                        if (test1Age == test2Age)
-                        {
-                            _logger.LogInformation("test1Age == test2Age");
-                        }
-                        _logger.LogInformation("Start ExecuteAsync");
-
-                        int.TryParse(_configuration.GetSection("DelayInMinutes").Value, out var delayInMinutes);
-                        int.TryParse(_configuration.GetSection("IterationCount").Value, out var iterationCount);
-                        var a = _testSettings.Get("Test2").TestAge;
-                        //for (var i = 0; i < iterationCount; i++)
-                        //{
-                        //    Console.WriteLine($"CommonConfig FirstName = {_appSettings.FirstName}, LastName = {_appSettings.LastName}");
-                        //    Console.WriteLine($"EnvironmentConfig FirstName = {_defaultStudent.FirstName}, LastName = {_defaultStudent.LastName}");
-                        //    Console.WriteLine(_calendar.GetCurrentDate());
-
-                        //    await Task.Delay(delayInMinutes, stoppingToken);
-                        //}
-                    }
-                }
-            }, cancellationToken);
-        }
-
-        public virtual Task StopAsync(CancellationToken cancellationToken)
-        {
-            _logger.LogInformation("Hosted service stopping");
-
+            _executingTask = ExecuteAsync(_stoppingCts.Token);
+            
             return Task.CompletedTask;
         }
+
+        public virtual async Task StopAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                _stoppingCts.Cancel();
+            }
+            finally
+            {
+                await Task.WhenAny(_executingTask, Task.Delay(Timeout.Infinite, cancellationToken));
+            }
+        }
+
+        protected virtual async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                using (_logger.BeginScope("Execute Async has started"))
+                {
+                    var root = (IConfigurationRoot)_configuration;
+                    var jsonProvider = root.Providers.ToArray()[1] as JsonConfigurationProvider;
+                    jsonProvider.TryGet("SomeValue", out string value);
+                    var someVAlue = _configuration.GetValue<int>("SomeValue");
+                    var timespan = new TimeSpan(0, 0, 0, 30);
+                    await Task.Delay(timespan, stoppingToken);
+                    var test2Age = _testSettings.Get("Test2").TestAge;
+                    var test1Age = _testSettings.Get("Test1").TestAge;
+                    if (test1Age == test2Age)
+                    {
+                        _logger.LogInformation("test1Age == test2Age");
+                    }
+                    _logger.LogInformation("Start ExecuteAsync");
+
+                    int.TryParse(_configuration.GetSection("DelayInMinutes").Value, out var delayInMinutes);
+                    int.TryParse(_configuration.GetSection("IterationCount").Value, out var iterationCount);
+                    var a = _testSettings.Get("Test2").TestAge;
+                    for (var i = 0; i < iterationCount; i++)
+                    {
+                        Console.WriteLine($"CommonConfig FirstName = {_appSettings.FirstName}, LastName = {_appSettings.LastName}");
+                        Console.WriteLine($"EnvironmentConfig FirstName = {_defaultStudent.FirstName}, LastName = {_defaultStudent.LastName}");
+                        Console.WriteLine(_calendar.GetCurrentDate());
+
+                        await Task.Delay(delayInMinutes, stoppingToken);
+                    }
+                }
+            }
+        }
+
 
         private void Listener(TestSettings obj)
         {
